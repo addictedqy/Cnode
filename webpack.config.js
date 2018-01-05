6999420678bb81efca9f6b04ef05458df97c6991
Addicted <Addicted@AddicteddeMacBook-Air.local>
2018-01-05 16:04:09 +0800

    initalize project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const PATH_SRC = __dirname + '/src';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: PATH_SRC + '/App.jsx',
+  output: {
+    path: __dirname + './dist',
+    filename: 'bundle.[hash].js',
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+    host: '0.0.0.0',
+    port: '8080',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Code: Node.js专业中文社区',
+      template: PATH_SRC + '/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react'],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'},
+        ],
+      },
+      {
+        test: '/\.(svg|jpg|git|png)$/',
+        loader: 'url-loader',
+      }
+    ]
+  }
+}
